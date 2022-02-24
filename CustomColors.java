4bff63f1a3bcf87df0af84a7d7426914a6459471
JingerJesus <71798517+JingerJesus@users.noreply.github.com>
2022-02-24 11:19:46 -0500

    First Iteration of colors
    Contains:
    White / LightGrey / MidGrey / DarkGrey / DarkerGrey / Black
    LightRed / MidRed / DarkRed / DarkDarkRed
    SkyBlue

---
+++ CustomColors.java
@@ -0,0 +1,24 @@
+package com.github.jingerjesus.simulationsimulator;
+
+import javafx.scene.paint.Color;
+
+public class CustomColors {
+    //colors I might need
+
+    //greys
+    public static Color white = Color.rgb(255,255,255);
+    public static Color lightGrey = Color.rgb(212, 212, 212);
+    public static Color midGrey = Color.rgb(171,171,171);
+    public static Color darkGrey = Color.rgb(64,64,64);
+    public static Color darkerGrey = Color.rgb(38,38,38);
+    public static Color black = Color.rgb(0,0,0);
+
+    //reds
+    public static Color lightRed = Color.rgb(186,0,0);
+    public static Color midRed = Color.rgb(140,0,0);
+    public static Color darkRed = Color.rgb(84,0,0);
+    public static Color darkDarkRed = Color.rgb(43,0,0);
+
+    //blues
+    public static Color skyBlue = Color.rgb(124, 176, 222);
+}
